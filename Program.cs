7cd71c94c7b472b5de455989c880648f3132523f
Ramirojj <126816654+Ramirojj@users.noreply.github.com>
2024-09-24 05:38:34 -0500

    [1.06]Adding Mario csv

--- Program.cs
+++ Program.cs
@@ -3,5 +3,14 @@ string path = Directory.GetCurrentDirectory() + "//nlog.config";
 // create instance of Logger
 var logger = LogManager.Setup().LoadConfigurationFromFile(path).GetCurrentClassLogger();
 logger.Info("Program started");
-Console.WriteLine("Hello World!");
+string file = "mario.csv";
+// make sure movie file exists
+if (!File.Exists(file))
+{
+    logger.Error("File does not exist: {File}", file);
+}
+else
+{
+    // TODO: create user menu
+}
 logger.Info("Program ended");
